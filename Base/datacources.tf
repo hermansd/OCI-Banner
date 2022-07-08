3f0f25ef02219a2c713d42682c41dc092b3134a6
Scott Herman <hermansd@Mac-mini.local>
2022-07-08 13:23:13 -0400

    Changes to networking

--- Base/datacources.tf
+++ Base/datacources.tf
@@ -9,3 +9,15 @@ data "oci_core_images" "supported_shape_images" {
 #    values = ["true"]
 #  }
 }
+
+
+data "oci_core_vcns" "this_vcn" {
+  compartment_id   = "ocid1.compartment.oc1..aaaaaaaawa6zpqbwer44tocvotkzytddtobk65h3vbo6g3juxzs2huz2sy7q"
+  #shape            = "VM.Standard.E3.Flex" #var.instance_shape
+  #operating_system = "Oracle Linux"
+
+#  filter {
+#    name   = "launch_options.is_pv_encryption_in_transit_enabled"
+#    values = ["true"]
+#  }
+}
