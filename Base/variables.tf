2c592949edb64652d52d1febbbdb6d4a9e21e7da
Scott Herman <hermansd@Mac-mini.local>
2022-07-08 16:22:16 -0400

    Changes to networking

--- Base/variables.tf
+++ Base/variables.tf
@@ -2,12 +2,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 # Environment
-variable "service_label" {
-  validation {
-    condition     = length(regexall("^[A-Za-z][A-Za-z0-9]{1,7}$", var.service_label)) > 0
-    error_message = "Validation failed for service_label: value is required and must contain alphanumeric characters only, starting with a letter up to a maximum of 8 characters."
-  }
-}
+variable "service_label" {}
 
 variable "target_compartment_ocid" {}
 variable "shape_name" {}
@@ -34,9 +29,4 @@ variable "private_key_path" {
 variable "private_key_password" {
   default = ""
 }
-variable "region" {
-  validation {
-    condition     = length(trim(var.region, "")) > 0
-    error_message = "Validation failed for region: value is required."
-  }
-}
+variable "region" {}
