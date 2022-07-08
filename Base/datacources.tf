af071e0fe99a15f574e9e7cf3b6ef8a2e55a95de
Scott Herman <hermansd@Mac-mini.local>
2022-07-08 13:19:25 -0400

    Changes to networking

--- Base/datacources.tf
+++ Base/datacources.tf
@@ -1,6 +1,6 @@
 # Gets a list of all Oracle Linux 7.5 images that support a given Instance shape
 data "oci_core_images" "supported_shape_images" {
-  compartment_id   = var.tenancy_ocid
+  compartment_id   = ${tenancy_ocid}
   shape            = "VM.Standard.E3.Flex" #var.instance_shape
   operating_system = "Oracle Linux"
 
