6fb1d2e5c9a382d2f7681143df0142364fb69100
Scott Herman <hermansd@Mac-mini-4.local>
2022-09-08 14:24:49 -0400

    Banner ENV Deployment

--- EnvDeploy/appServers.tf
+++ EnvDeploy/appServers.tf
@@ -5,7 +5,7 @@
 resource "oci_core_instance" "appServer" {
   count               = var.host_count
   availability_domain = var.multiple_ads ? (contains(range(1,var.host_count , 2), count.index) ? var.target_ad2 : var.target_ad1) : var.target_ad1
-  fault_domain        = "FAULT-DOMAIN-${random_integer.fault_domain.result}"
+  fault_domain        = "FAULT-DOMAIN-${floor(count.index / var.host_count)}"
   compartment_id      = var.target_compartment_ocid
   display_name        = "${var.service_label}app${count.index}"
   shape               = var.shape_name
@@ -41,15 +41,4 @@ resource "oci_core_instance" "appServer" {
   #timeouts {
   #  create = "60m"
   #}
-}
-
-
-
-resource "random_integer" "fault_domain" {
-  min = 1
-  max = 3
-  keepers = {
-    # Generate a new id each time we switch to a new AMI id
-    fd_id = oci_core_instance.appServer.*.id[count.index]
-  }
 }
