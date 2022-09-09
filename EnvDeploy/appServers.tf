05eeb488a3c0f80237e0cc6f5a7036a625356ab1
Scott Herman <hermansd@Mac-mini-4.local>
2022-09-09 13:26:15 -0400

    Adding jobsub server

--- EnvDeploy/appServers.tf
+++ EnvDeploy/appServers.tf
@@ -3,10 +3,10 @@
 
 
 resource "oci_core_instance" "appServer" {
-  count               = var.host_count
-  availability_domain = var.app_multiple_ads ? (contains(range(1,var.app_host_count , 2), count.index) ? var.target_ad2 : var.target_ad1) : var.target_ad1
+  count               = var.app_host_count
+  availability_domain = var.app_multiple_ads ? (contains(range(1,var.app_host_count , 2), count.index) ? var.app_target_ad2 : var.target_ad1) : var.target_ad1
   fault_domain        = var.app_multiple_fds ? ((contains(range(1,var.app_host_count , 2), count.index)) ? var.app_target_fd2 : var.app_target_fd1) : element(var.fault_domain,count.index)
-  compartment_id      = var.app_compartment_ocid
+  compartment_id      = var.target_compartment_ocid
   display_name        = "${var.app_service_label}app${count.index}"
   shape               = var.app_shape_name
 
@@ -24,7 +24,7 @@ resource "oci_core_instance" "appServer" {
 
   source_details {
     source_type = "image"
-    source_id   = var.app_change_ssh ? var.app_sshkey : var.sshkey
+    source_id   = var.app_os_image_ocid
 
   }
 
@@ -34,7 +34,7 @@ resource "oci_core_instance" "appServer" {
   #preserve_boot_volume = true
 
   metadata = {
-    ssh_authorized_keys = var.app_sshkey
+    ssh_authorized_keys = var.app_change_ssh ? var.app_sshkey : var.sshkey
   }
 
   #defined_tags = var.defined_tags
