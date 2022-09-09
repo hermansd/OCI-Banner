62a5a4bff1db8b196df57c81b904d743edce7afc
Scott Herman <hermansd@Mac-mini-4.local>
2022-09-09 13:30:58 -0400

    Adding jobsub server

--- EnvDeploy/appServers.tf
+++ EnvDeploy/appServers.tf
@@ -7,7 +7,7 @@ resource "oci_core_instance" "appServer" {
   availability_domain = var.app_multiple_ads ? (contains(range(1,var.app_host_count , 2), count.index) ? var.app_target_ad2 : var.target_ad1) : var.target_ad1
   fault_domain        = var.app_multiple_fds ? ((contains(range(1,var.app_host_count , 2), count.index)) ? var.app_target_fd2 : var.app_target_fd1) : element(var.fault_domain,count.index)
   compartment_id      = var.target_compartment_ocid
-  display_name        = "${var.app_service_label}app${count.index}"
+  display_name        = "${var.service_label}app${count.index}"
   shape               = var.app_shape_name
 
   shape_config {
@@ -17,9 +17,9 @@ resource "oci_core_instance" "appServer" {
 
   create_vnic_details {
     subnet_id        = var.app_subnet_ocid
-    display_name     = "${var.app_service_label}app${count.index}"
+    display_name     = "${var.service_label}app${count.index}"
     assign_public_ip = var.app_assign_public_ip
-    hostname_label   = "${var.app_service_label}app${count.index}"
+    hostname_label   = "${var.service_label}app${count.index}"
   }
 
   source_details {
