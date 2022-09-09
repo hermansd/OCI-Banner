3e77158f6da0ba10f143b213d1c6fb61e53fd41a
Scott Herman <hermansd@Mac-mini-4.local>
2022-09-08 21:11:32 -0400

    Banner ENV Deployment

--- EnvDeploy/appServers.tf
+++ EnvDeploy/appServers.tf
@@ -9,7 +9,7 @@ locals{
 resource "oci_core_instance" "appServer" {
   count               = var.host_count
   availability_domain = var.multiple_ads ? (contains(range(1,var.host_count , 2), count.index) ? var.target_ad2 : var.target_ad1) : var.target_ad1
-  fault_domain        = element(locals.fault_domain,count.index)
+  fault_domain        = element(local.fault_domain,count.index)
   compartment_id      = var.target_compartment_ocid
   display_name        = "${var.service_label}app${count.index}"
   shape               = var.shape_name
